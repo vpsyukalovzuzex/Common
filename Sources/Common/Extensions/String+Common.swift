cf6c6657725db05389fc5866c3453849514c0aa2
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-09-13 12:36:50 +0300

    5

--- Sources/Common/Extensions/String+Common.swift
+++ Sources/Common/Extensions/String+Common.swift
@@ -9,4 +9,12 @@ public extension String {
     var localizable: String {
         return NSLocalizedString(self, comment: "")
     }
+    
+    func ranges(of substring: String, options: CompareOptions = [], locale: Locale? = nil) -> [Range<Index>] {
+        var ranges = [Range<Index>]()
+        while ranges.last.map({ $0.upperBound < endIndex }) ?? true, let range = range(of: substring, options: options, range: (ranges.last?.upperBound ?? startIndex)..<endIndex, locale: locale) {
+            ranges.append(range)
+        }
+        return ranges
+    }
 }
